eed25543c3f8458bc25f9ff1f863567fef4beb6d
JuliaMieszaniec <juliamieszaniec@gmail.com>
2022-10-12 19:00:19 +0200

    zadania 1

---
+++ 01-CourseIntroduction/zadanie3.py
@@ -0,0 +1,4 @@
+x=input(str('Podaj liczbę godzin:'))
+y=input(str('Podaj stawke godzinową:'))
+z=float(x)*float(y)
+print(('Wynagrodzenie: '),(z))
---
+++ 01-CourseIntroduction/zadanie5.py
@@ -0,0 +1,3 @@
+temp=input('Podaj temerature w celcujszach:')
+temp= float(temp) *9 / 5 + 32
+print(('Temperatura w Farenheitach:'),(temp))
