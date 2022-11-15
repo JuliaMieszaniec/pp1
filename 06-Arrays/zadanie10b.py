d33fe8bcf819e7633332c085bddc7f0940f8ffb6
JuliaMieszaniec <juliamieszaniec@gmail.com>
2022-11-15 22:13:53 +0100

    zadania

--- 06-Arrays/zadanie10b.py
+++ 06-Arrays/zadanie10b.py
@@ -6,6 +6,22 @@ for y in li:
     while (y%2==0):     
         even+=1
 
-
-
 print(f'even1:{even} odd1:{odd}')
+
+
+list1 = [10, 21, 4, 45, 66, 93, 1]
+ 
+even_count, odd_count = 0, 0
+ 
+# iterating each number in list
+for num in list1:
+ 
+    # checking condition
+    if num % 2 == 0:
+        even_count += 1
+ 
+    else:
+        odd_count += 1
+ 
+print("Even numbers in the list: ", even_count)
+print("Odd numbers in the list: ", odd_count)
