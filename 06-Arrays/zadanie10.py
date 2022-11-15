e28fa9618dfa5a4464102faf9f9be48d9782ee80
JuliaMieszaniec <juliamieszaniec@gmail.com>
2022-11-15 22:10:00 +0100

    zadania

---
+++ 06-Arrays/zadanie10.py
@@ -0,0 +1,14 @@
+li=[1,2,3,4,5,6,7,8,9]
+even=0
+odd=0
+
+for x in li:
+    if x%2==0:
+        even+=1
+    else:
+        odd+=1
+
+print(f'even:{even} odd:{odd}')
+
+
+
---
+++ 06-Arrays/zadanie10b.py
@@ -0,0 +1,11 @@
+li=[1,2,3,4,5,6,7,8,9]
+even=0
+odd= int(len(li)-even)
+
+for y in li:
+    while (y%2==0):     
+        even+=1
+
+
+
+print(f'even1:{even} odd1:{odd}')
