ddff1a154112b2a7a72f2371b91694ce659893cb
JuliaMieszaniec <juliamieszaniec@gmail.com>
2022-11-29 15:13:03 +0100

    zadanie

--- 07-FileHandling/zadanie22.py
+++ 07-FileHandling/zadanie22.py
@@ -1,6 +1,9 @@
 import csv
 
-with open('students.txt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+with open('students.txt', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
     for row in csv_reader:
-        print(row)
+        if int(row[2])<30:
+            print(row[0],row[1],row[4])
+            
